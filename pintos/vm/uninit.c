653c5b0540e742297dd434abf372403c49ff85e8
EUNTELLA <yokohamay0612@gmail.com>
2025-10-11 13:32:14 +0900

    unused 제거

--- pintos/vm/uninit.c
+++ pintos/vm/uninit.c
@@ -56,7 +56,7 @@ static bool uninit_initialize(struct page *page, void *kva) {
  * exit, which are never referenced during the execution.
  * PAGE will be freed by the caller. */
 static void uninit_destroy(struct page *page) {
-  struct uninit_page *uninit UNUSED = &page->uninit;
+  struct uninit_page *uninit = &page->uninit;
   if (uninit->aux != NULL) {
     free(uninit->aux);
   }
