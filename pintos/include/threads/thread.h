267ef5b0b05560c4e84bdb4ab5dd49b8f8ad6b83
A Yeon Jang <125449701+ayeon59@users.noreply.github.com>
2025-10-11 20:05:51 +0900

    [refactor] redefine rsp in thread struct

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -128,7 +128,7 @@ struct thread {
 #ifdef VM
   /* Table for whole virtual memory owned by thread. */
   struct supplemental_page_table spt;
-  void *rsp;
+  void *user_rsp;
 #endif
 
   /* Owned by thread.c. */
